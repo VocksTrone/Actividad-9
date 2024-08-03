2cc5d01bdf58b2e9fc82e8025b56082d46ed3ca2
VocksTrone <jr.alvarez0316@gmail.com>
2024-08-02 19:52:04 -0600

    Programa Completo

--- Actividad#9/Accounts.cs
+++ Actividad#9/Accounts.cs
@@ -8,5 +8,163 @@ namespace Actividad_9
 {
     internal class Accounts
     {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Amount { get; set; }
+        public static void AgregarCuenta(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            Console.WriteLine("\nIngrese el ID de la Cuenta");
+            string id = Console.ReadLine();
+            Console.WriteLine("\nIngrese el Nombre de la Cuenta");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nIngrese el Tipo de Cuenta (Monetaria | Ahorro)");
+            string type = Console.ReadLine();
+            Console.WriteLine("\nIngrese el Monto Inicial de la Cuenta");
+            double amount = double.Parse(Console.ReadLine());
+            Accounts addAccount = accountsList.Find(p => p.ID == id);
+            if (addAccount == null)
+            {
+                Console.WriteLine("\nCuenta Añadida Exitosamente");
+                Accounts newAccount = new Accounts(id, name, type, amount);
+                accountsList.Add(newAccount);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nLa Cuenta Ya Existe");
+                Console.ReadKey();
+            }
+        }
+        public static void Depositar(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            Console.WriteLine("\nIngrese el ID de la Cuenta");
+            string id = Console.ReadLine();
+            Accounts addAccount = accountsList.Find(p => p.ID == id);
+            if (addAccount != null)
+            {
+                Console.WriteLine("\nCuenta Encontrada");
+                Console.WriteLine("\nIngrese la Cantidad a Depositar");
+                double deposit = double.Parse(Console.ReadLine());
+                addAccount.Amount = Math.Round(addAccount.Amount + deposit, 2);
+                Console.WriteLine("\nDepósito Realizado con Éxito");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nLa Cuenta No Existe");
+                Console.ReadKey();
+            }
+        }
+        public static void Retirar(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            Console.WriteLine("\nIngrese el ID de la Cuenta");
+            string id = Console.ReadLine();
+            Accounts addAccount = accountsList.Find(p => p.ID == id);
+            if (addAccount != null)
+            {
+                Console.WriteLine("\nCuenta Encontrada");
+                Console.WriteLine("\nIngrese la Cantidad a Retirar");
+                double withdraw = double.Parse(Console.ReadLine());
+                if (withdraw <= addAccount.Amount)
+                {
+                    addAccount.Amount = Math.Round(addAccount.Amount - withdraw, 2);
+                    Console.WriteLine("\nRetiro Realizado con Éxito");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("\nEl Monto de la Cuenta es Insuficiente");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nLa Cuenta No Existe");
+                Console.ReadKey();
+            }
+        }
+        public static void Modificar(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            Console.WriteLine("\nIngrese el ID de la Cuenta");
+            string id = Console.ReadLine();
+            Accounts addAccount = accountsList.Find(p => p.ID == id);
+            if (addAccount != null)
+            {
+                Console.WriteLine("\nCuenta Encontrada");
+                Console.WriteLine("\nIngrese la Nuevo Tipo de Cuenta (Monetaria | Ahorro)");
+                string newType = Console.ReadLine();
+                addAccount.Type = newType;
+                Console.WriteLine("\nEl Tipo de Cuenta fue Actualizado con Éxito");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nLa Cuenta No Existe");
+                Console.ReadKey();
+            }
+        }
+        public static void ConsultarSaldo(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            Console.WriteLine("\nIngrese el ID de la Cuenta");
+            string id = Console.ReadLine();
+            Accounts addAccount = accountsList.Find(p => p.ID == id);
+            foreach (Accounts accountAdd in accountsList)
+            {
+                if (addAccount.ID == id)
+                {
+                    Console.WriteLine($"\nEl Saldo de la Cuenta es: Q.{Math.Round(accountAdd.Amount, 2)}");
+                    Console.ReadKey(); 
+                }
+            }
+        }
+        public static void MostrarDatos(ref List<Accounts> accountsList)
+        {
+            Console.Clear();
+            Banco();
+            if (accountsList.Count == 0)
+            {
+                Console.WriteLine("\nNo Existen Cuentas Registradas");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nIngrese el ID de la Cuenta");
+                string id = Console.ReadLine();
+                Accounts addAccount = accountsList.Find(p => p.ID == id);
+                foreach (Accounts accountAdd in accountsList)
+                {
+                    if (accountAdd.ID == id)
+                    {
+                        Console.WriteLine($"\nID: {accountAdd.ID}");
+                        Console.WriteLine($"\nNombre: {accountAdd.Name}");
+                        Console.WriteLine($"\nTipo de Cuenta: {accountAdd.Type}");
+                        Console.WriteLine($"\nMonto: Q.{accountAdd.Amount}"); 
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+        public static void Banco()
+        {
+            Console.WriteLine("Bienvenido al Banco 'El Patrón'");
+        }
+        public Accounts(string id, string name, string type, double amount)
+        {
+            ID = id;
+            Name = name;
+            Type = type;
+            Amount = amount;
+        }
     }
 }
--- Actividad#9/Program.cs
+++ Actividad#9/Program.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using Actividad_9;
+
+bool generalContinue = true;
+List <Accounts> accountsList = new List <Accounts> ();
+while (generalContinue)
+{
+    try
+    {
+        MostrarSwitch(ref accountsList, ref generalContinue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("\nError!, Datos Incorrectos");
+        Console.ReadKey();
+    }
+}
+static int MostrarMenu()
+{
+    Console.Clear();
+    Console.WriteLine("1. Agregar Cuenta");
+    Console.WriteLine("2. Depositar");
+    Console.WriteLine("3. Retirar");
+    Console.WriteLine("4. Modificar Tipo de Cuenta");
+    Console.WriteLine("5. Consultar Saldo");
+    Console.WriteLine("6. Mostrar Datos");
+    Console.WriteLine("7. Salir");
+    int menuOption = int.Parse(Console.ReadLine());
+    return menuOption;
+}
+static bool Salir(ref bool generalContinue)
+{
+    Console.Clear();
+    Console.WriteLine("Usted está Saliendo del Programa");
+    generalContinue = false;
+    return generalContinue;
+}
+static void MostrarSwitch(ref List<Accounts> accountsList, ref bool generalContinue)
+{
+    switch (MostrarMenu())
+    {
+        case 1:
+            Accounts.AgregarCuenta(ref accountsList);
+            break;
+        case 2:
+            Accounts.Depositar(ref accountsList);
+            break;
+        case 3:
+            Accounts.Retirar(ref accountsList);
+            break;
+        case 4:
+            Accounts.Modificar(ref accountsList);
+            break;
+        case 5:
+            Accounts.ConsultarSaldo(ref accountsList);
+            break;
+        case 6:
+            Accounts.MostrarDatos(ref accountsList);
+            break;
+        case 7:
+            Salir(ref generalContinue);
+            break;
+        default:
+            Console.WriteLine("Ingrese una Opción Válida (1 - 7)");
+            Console.ReadKey();
+            break;
+    }
+}
